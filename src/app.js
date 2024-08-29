0684151f80924b88e0b7bef2057f810f2af7c819
AudreyShelton92 <83794598+AudreyShelton92@users.noreply.github.com>
2024-08-29 00:25:20 +0000

    Generator

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { Button } from "bootstrap";
 
 window.onload = function() {
   //write your code here
@@ -12,45 +13,57 @@ window.onload = function() {
 
 const generateExcuse = () => {
   const allFirstPart = [
-      "My chickens", 
-      "My baby", 
-      "My wrinkly grandma", 
-      "My stinky neighbor", 
-      "The nosy neighbor", 
-      "The neighbor's mistress", 
-      "The mayor", 
-      "Horton the elephant"
+    "My chickens",
+    "My baby",
+    "My wrinkly grandma",
+    "My stinky neighbor",
+    "The nosy lady next door",
+    "The guy at the bus stop",
+    "The mayor",
+    "Horton the elephant"
   ];
 
   const allSecondPart = [
-      "dropped", 
-      "jumped on", 
-      "laughed", 
-      "died", 
-      "forgot", 
-      "interrupted", 
-      "ate"
+    "dropped",
+    "jumped on",
+    "laughed about",
+    "didn't listen to me about",
+    "forgot",
+    "interrupted",
+    "ate"
   ];
 
   const allLastPart = [
-      "trying to disco",
-      "my lunch",
-      "my car",
-      "my new computer",
-      "my Snickers bar",
-      "my rock collection"
+    "my brand new scooter",
+    "my lunch",
+    "my car",
+    "my new computer",
+    "my Snickers bar",
+    "my rock collection"
   ];
 
   const firstIndex = Math.floor(Math.random() * allFirstPart.length);
   const secondIndex = Math.floor(Math.random() * allSecondPart.length);
   const lastIndex = Math.floor(Math.random() * allLastPart.length);
 
-  const excuse = allFirstPart[firstIndex] + " " + allSecondPart[secondIndex] + " " + allLastPart[lastIndex] + ".";
+  const excuse =
+    allFirstPart[firstIndex] +
+    " " +
+    allSecondPart[secondIndex] +
+    " " +
+    allLastPart[lastIndex] +
+    ".";
 
   return excuse;
 };
 
+const excuseElement = document.querySelector(".Excuse");
+if (excuseElement) {
+  excuseElement.innerHTML = generateExcuse();
+}
 
-document.querySelector(".Excuse").innerHTML = fullExcuse;
+document.querySelector("button").addEventListener("click", () => {
+  document.querySelector(".Excuse").innerHTML = generateExcuse();
+});
 
 console.log(generateExcuse());
